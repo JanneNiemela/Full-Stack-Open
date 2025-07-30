cfaeadc31b1b792bcc6a322b56b6064676f50518
JanneNiemela <janneniemela90@gmail.com>
2025-07-31 02:07:43 +0300

    Complete exercise 2.13.

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import personService from './services/persons'
 
 const App = () => {
   const [persons, setPersons] = useState([])
@@ -11,10 +11,10 @@ const App = () => {
   const [filter, setFilter] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
+    personService
+      .getAll()
+      .then(serverPersons => {
+        setPersons(serverPersons)
       })
   }, [])
 
@@ -37,10 +37,10 @@ const App = () => {
       phoneNumber: newPhoneNumber
     }
 
-    axios
-      .post('http://localhost:3001/persons', person)
-      .then(response => {
-        setPersons(persons.concat(response.data))
+    personService
+      .create(person)
+      .then(serverPerson => {
+        setPersons(persons.concat(serverPerson))
         setNewName('')
         setNewPhoneNumber('')
       })
---
+++ osa2/puhelinluettelo/src/services/persons.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+const url = 'http://localhost:3001/persons'
+
+const getAll = () => {
+  const request = axios.get(url)
+  return request.then(response => response.data)
+}
+
+const create = newObj => {
+  const request = axios.post(url, newObj)
+  return request.then(response => response.data)
+}
+
+export default { getAll, create }
