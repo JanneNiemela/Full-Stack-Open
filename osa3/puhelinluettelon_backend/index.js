b36f9553bf142c4e41eaf437c69f4b15caa84673
JanneNiemela <janneniemela90@gmail.com>
2025-09-03 13:19:58 +0300

    Complete exercises 3.15, 3.16 and 3.18.

--- osa3/puhelinluettelon_backend/index.js
+++ osa3/puhelinluettelon_backend/index.js
@@ -14,34 +14,47 @@ const generateRandomId = () => {
   return String(Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
 }
 
-app.get('/info', (request, response) => {
-  Person.collection.countDocuments().then((count) => {
-    response.send
-    (`
-      <p>Phonebook has info for ${count} people.</p>
-      <p>${new Date().toLocaleString()}</p>
-    `)
-  });
+app.get('/info', (request, response, next) => {
+  Person.collection.countDocuments()
+    .then((count) => {
+      response.send
+      (`
+        <p>Phonebook has info for ${count} people.</p>
+        <p>${new Date().toLocaleString()}</p>
+      `)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons', (request, response) => {
-  Person.find({}).then(persons => {
-    response.json(persons)
-  })
+app.get('/api/persons', (request, response, next) => {
+  Person.find({})
+    .then(persons => {
+      response.json(persons)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(person => {
-    response.json(person)
-  })
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  // TODO: add a delete feature.
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   
   if (!body.name || body.name.length === 0) {
@@ -60,10 +73,30 @@ app.post('/api/persons', (request, response) => {
     number: body.number,
   })
 
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'Unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'Malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
