910435324faf576fabb971d592ce3e10ec671fd1
JanneNiemela <janneniemela90@gmail.com>
2025-09-11 23:56:59 +0300

    Complete exercise 5.7.

--- osa5/blogilista-frontend/src/components/Blog.jsx
+++ osa5/blogilista-frontend/src/components/Blog.jsx
@@ -1,7 +1,45 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const style = {
+    padding: 5,
+    border: 'solid',
+    borderWidth: 1,
+    margin: 5,
+  }
+
+  const detailStyle = { display: showDetails ? '' : 'none' }
+  const toggleButtonText = (showDetails) ? 'Hide' : 'View'
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
+  const like = () => {
+    console.log('Like button pressed.')
+  }
+
+  return (
+    <div style={style}>
+      <div>
+        {blog && blog.title && blog.title.length > 0 && (<>{blog.title}</>)}
+        {blog && blog.author && blog.author.length > 0 && (<> by {blog.author}</>)}
+        <button onClick={toggleDetails}>{toggleButtonText}</button>
+      </div>
+      <div style={detailStyle}>
+        {blog && blog.url && blog.url.length > 0 && (<div>URL: {blog.url}</div>)}
+        {blog && blog.likes !== null && blog.likes !== undefined && (
+          <div>
+            Likes: {blog.likes}
+            <button onClick={like}>Like</button>
+          </div>
+        )}
+        {blog && blog.user && blog.user.name && blog.user.name.length > 0 && (<div>Added by: {blog.user.name}</div>)}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
