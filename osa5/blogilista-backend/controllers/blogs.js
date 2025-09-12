64e1d08d6d0c50039fca684ddadd5c5e1d46ead3
JanneNiemela <janneniemela90@gmail.com>
2025-09-12 17:59:32 +0300

    Make the backend return a blog with full user info when creating a new blog.

--- osa5/blogilista-backend/controllers/blogs.js
+++ osa5/blogilista-backend/controllers/blogs.js
@@ -22,7 +22,8 @@ blogsRouter.post('/', userExtractor, async (request, response) => {
   const savedBlog = await blog.save()
   user.blogs = user.blogs.concat(savedBlog._id)
   await user.save()
-  response.status(201).json(savedBlog)
+  const updatedPopulatedBlog = await savedBlog.populate('user', { username: 1, name: 1, id: 1 })
+  response.status(201).json(updatedPopulatedBlog)
 })
 
 blogsRouter.put('/:id', userExtractor, async (request, response) => {
